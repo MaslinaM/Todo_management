23cf8ec92d3994d280e15a91149989f3172c7c78
MaslinaM <maslina2904001@gmail.com>
2025-02-04 14:10:31 +0530

    first commit

---
+++ springboot1webapp/src/main/java/com/mylearning/springboot/myfirstwebapp/Springboot1webappApplication.java
@@ -0,0 +1,13 @@
+package com.mylearning.springboot.myfirstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot1webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot1webappApplication.class, args);
+	}
+
+}
---
+++ springboot1webapp/src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title>My first jsp html Page</title>
+	</head>
+	<body>
+		my page using jsp using html
+	</body>
+</html>
